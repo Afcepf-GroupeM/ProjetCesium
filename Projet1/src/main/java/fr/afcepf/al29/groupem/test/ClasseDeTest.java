a3e3c35e6843edf00eb51796cf06ea63360e9643
Guiguiolive <guiguiolive+github@gmail.com>
2016-12-09 18:17:12 +0100

    Test Guillaume Licorne

--- Projet1/src/main/java/fr/afcepf/al29/groupem/test/ClasseDeTest.java
+++ Projet1/src/main/java/fr/afcepf/al29/groupem/test/ClasseDeTest.java
@@ -3,7 +3,7 @@ package fr.afcepf.al29.groupem.test;
 public class ClasseDeTest {
 
 	public static void main(String[] args) {
-		System.out.println("Lol!");
+		System.out.println("Licorne Sharkounet");
 
 	}
 
