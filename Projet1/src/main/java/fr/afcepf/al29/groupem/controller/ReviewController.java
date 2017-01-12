0e2d46c459ee4431091637827e3b98744646ba33
pfrancois <Stagiaire@AFCEPF-G2KCL275>
2017-01-12 19:19:23 +0100

    createReview xhtml en cours

--- Projet1/src/main/java/fr/afcepf/al29/groupem/controller/ReviewController.java
+++ Projet1/src/main/java/fr/afcepf/al29/groupem/controller/ReviewController.java
@@ -1,5 +1,9 @@
 package fr.afcepf.al29.groupem.controller;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 
@@ -21,7 +25,7 @@ import fr.afcepf.al29.groupem.entities.User;
 public class ReviewController {
 
 	private int id;
-	private float rating;
+	private String rating;
 	private String comment;
 	private Date dateCreation;
 	private User user;
@@ -59,14 +63,29 @@ public class ReviewController {
 	}
 	
 	public String createReview(){
-		RegexValidator ratingValidator = new RegexValidator("^[0-9A-Za-z\\s-'éèà]*$", false);
-		RegexValidator commentValidator = new RegexValidator("^\\d+$", false);
+		RegexValidator ratingValidator = new RegexValidator("^([0-4]\\.[05])|([0-4],[05])|([0-5])$", false);
+		RegexValidator commentValidator = new RegexValidator("^[A-Za-z\\s-'éèà]*$", false);
+		
+		boolean ratingValid = ratingValidator.isValid(rating) && (!rating.isEmpty());
+		boolean commentValid = commentValidator.isValid(comment) && (!comment.isEmpty());
+		
+		message = "";
+		if(!ratingValid){message += "La note attribuée au produit est invalide!<br/>";}
+		if(!commentValid){message += "le commentaire contient un ou plusieurs charactères invalides!<br/>";}
+		
+		if(ratingValid && commentValid){
+			float ratingNumber = Float.parseFloat(rating);
+			Date creationDate = new Date();
+			
+			reviewBus.createReview(ratingNumber, comment, creationDate, item, user);
+			return "article.jsf?itemId=" + item.getId() + "?faces-redirect=true";
+		}
 		
 		return null;
 	}
 	
 	public void resetFields(){
-		rating = 0.0f;
+		rating = "";
 		comment = "";
 	}
 
@@ -78,11 +97,11 @@ public class ReviewController {
 		this.id = id;
 	}
 
-	public float getRating() {
+	public String getRating() {
 		return rating;
 	}
 
-	public void setRating(float rating) {
+	public void setRating(String rating) {
 		this.rating = rating;
 	}
 
