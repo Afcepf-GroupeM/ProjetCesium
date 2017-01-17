c58ae8c12d214aecb3ff45137d3be6c37a6f514a
Stagiaire <Stagiaire@AFCEPF-LTGMIM2O>
2017-01-17 18:25:10 +0100

    Haiyan html admin item

---
+++ Projet1/src/main/java/fr/afcepf/al29/groupem/controller/AdminAddItemController.java
@@ -0,0 +1,22 @@
+package fr.afcepf.al29.groupem.controller;
+
+import java.util.List;
+
+import fr.afcepf.al29.groupem.entities.Category;
+import fr.afcepf.al29.groupem.entities.MetaCategory;
+
+public class AdminAddItemController {
+	private String name;
+	private String reference;
+	private List<MetaCategory> listeMetaCategorie;
+	private MetaCategory metaCategorie;
+	private List<Category> ListeCategory;
+	private Category category;
+	private double price;
+//	private TypeMoney typeMoney;
+//	private List<TypeMoney> listeMoney;
+	private int quantity;
+	private String urlImage;
+	private String message;
+
+}
