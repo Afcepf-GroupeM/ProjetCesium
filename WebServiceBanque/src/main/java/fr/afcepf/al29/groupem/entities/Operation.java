c989301bd40c03726f18d485719ce3d5392c635b
Stagiaire <Stagiaire@192.168.102.106>
2017-01-24 15:36:59 +0100

    Haiyan WS banque Operation

--- WebServiceBanque/src/main/java/fr/afcepf/al29/groupem/entities/Operation.java
+++ WebServiceBanque/src/main/java/fr/afcepf/al29/groupem/entities/Operation.java
@@ -1,35 +1,15 @@
 
-import java.util.*;
-
-/**
- * 
- */
 public class Operation {
 
-    /**
-     * Default constructor
-     */
     public Operation() {
     }
 
-    /**
-     * 
-     */
     private Integer id;
 
-    /**
-     * 
-     */
     private String label;
 
-    /**
-     * 
-     */
     private BigDecimal amount;
 
-    /**
-     * 
-     */
     private Date dateOp;
 
 
