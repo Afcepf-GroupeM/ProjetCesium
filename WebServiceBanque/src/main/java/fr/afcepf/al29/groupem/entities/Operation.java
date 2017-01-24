f28cccb9f47821b7d120344066e8b4a1f7de312d
Charline <Stagiaire@192.168.102.97>
2017-01-24 15:31:51 +0100

    Modifications entités : webservice banque

--- WebServiceBanque/src/main/java/fr/afcepf/al29/groupem/entities/Operation.java
+++ WebServiceBanque/src/main/java/fr/afcepf/al29/groupem/entities/Operation.java
@@ -1,36 +1,70 @@
+package fr.afcepf.al29.groupem.entities;
 
-import java.util.*;
+import java.math.BigDecimal;
+import java.util.Date;
 
-/**
- * 
- */
+
+
 public class Operation {
 
-    /**
-     * Default constructor
-     */
+   
     public Operation() {
     }
 
-    /**
-     * 
-     */
+   
     private Integer id;
 
-    /**
-     * 
-     */
+  
     private String label;
 
-    /**
-     * 
-     */
+   
     private BigDecimal amount;
 
-    /**
-     * 
-     */
+    
     private Date dateOp;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getLabel() {
+		return label;
+	}
+
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+
+	public Date getDateOp() {
+		return dateOp;
+	}
+
+
+	public void setDateOp(Date dateOp) {
+		this.dateOp = dateOp;
+	}
+    
+    
+    
 
 
 }
