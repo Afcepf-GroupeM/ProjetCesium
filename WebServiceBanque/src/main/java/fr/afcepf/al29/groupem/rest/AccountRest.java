a2d13d314157c767c884a5c1b0e782c2b6882e91
Charline <Stagiaire@192.168.102.97>
2017-01-25 12:47:49 +0100

    Modif

--- WebServiceBanque/src/main/java/fr/afcepf/al29/groupem/rest/AccountRest.java
+++ WebServiceBanque/src/main/java/fr/afcepf/al29/groupem/rest/AccountRest.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 public class AccountRest {
+	
 	public void verifyAccount(Integer numberCarte,Date dateExpiredCarte,Integer crytogram,String lastName,BigDecimal balance){
+		
 		
 	}
 
