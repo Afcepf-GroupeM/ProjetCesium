9014d9028118e3d963a4181467279a12dac15e36
Stagiaire <Stagiaire@192.168.102.106>
2017-01-25 16:33:51 +0100

    Haiyan AccountRest, ResponseBank

--- WebServiceBanque/src/main/java/fr/afcepf/al29/groupem/rest/AccountRest.java
+++ WebServiceBanque/src/main/java/fr/afcepf/al29/groupem/rest/AccountRest.java
@@ -10,15 +10,22 @@ import javax.ws.rs.Produces;
 
 import fr.afcepf.al29.groupem.business.AccountBusApi;
 import fr.afcepf.al29.groupem.business.AccountBusImpl;
+import fr.afcepf.al29.groupem.entities.ResponseBank;
 
 @Path("UserAccountService")
 public class AccountRest {
 	private AccountBusApi accountBusApi;
+	
 	@GET
 	@Produces("text/plain")
-	@Path("euroToFranc/{montant}")
-	public void verifyAccount(@PathParam("numberCard")String numberCard,@PathParam("dateExpiredCarte") Date dateExpiredCarte,@PathParam("crytogram") String crytogram,@PathParam("lastName") String lastName,@PathParam("balance") BigDecimal balance){
-		accountBusApi.getIdByNumberCard(numberCard);
+	@Path("verifyAccount/{numberCard,dateExpiredCarte,crytogram,lastName,balance}")
+	public ResponseBank receptionInfoReturnResponse(@PathParam("numberCard")String numberCard,@PathParam("dateExpiredCarte") Date dateExpiredCarte,@PathParam("crytogram") String crytogram,@PathParam("lastName") String lastName,@PathParam("balance") BigDecimal balance){
+		return null;
+		
+	}
+		
+	public Integer verifyAccount(@PathParam("numberCard")String numberCard,@PathParam("dateExpiredCarte") Date dateExpiredCarte,@PathParam("crytogram") String crytogram,@PathParam("lastName") String lastName,@PathParam("balance") BigDecimal balance){
+		return accountBusApi.getIdByNumberCard(numberCard);
 	}
 
 }
