293b435165bfe0b194e8f048643a88ca1208a1a9
Charline <Stagiaire@192.168.102.97>
2017-01-25 10:26:12 +0100

    Insert base de données banque

---
+++ WebServiceBanque/src/main/database/InsertBanque.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO customer (id,civilite,lastname,firstname,birthday,email,telephone,address,city,country,zipcode) VALUES (1,'Mr','Lagaffe','Gaston','1956-10-02','toto@franquin.com','0163458950','40 rue des champs','paris','france','75000');
+
+INSERT INTO account (id,description,balance,typeCarte,numberCard,crytogram,MONTANT_OPDEBIT,dateExpiredCarte,dateCreationAccount,customerid) VALUES (1,'compte ou il y a de argent','10000','CarteBleue','123456789','123','7','2017-12-01','2014-10-01',1);
+
+INSERT INTO operation (id,label,amount,accountid) VALUES (1,'achat d un ordinateur','500',1);
