877543df81f3e13c3d1357d230435fe657c52179
pfrancois <Stagiaire@192.168.102.77>
2017-01-30 19:23:14 +0100

    statut viewer en cours

--- Projet1/src/main/java/fr/afcepf/al29/groupem/controller/EspaceClientController.java
+++ Projet1/src/main/java/fr/afcepf/al29/groupem/controller/EspaceClientController.java
@@ -1,11 +1,25 @@
 package fr.afcepf.al29.groupem.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ComponentSystemEvent;
+
+import org.apache.myfaces.view.facelets.el.RedirectMethodExpressionValueExpressionValueChangeListener;
+import org.primefaces.json.JSONArray;
+import org.primefaces.json.JSONException;
+import org.primefaces.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -13,6 +27,7 @@ import org.springframework.stereotype.Component;
 import fr.afcepf.al29.groupem.business.api.AddressBusApi;
 import fr.afcepf.al29.groupem.business.api.OrderBusApi;
 import fr.afcepf.al29.groupem.business.api.UserBusApi;
+import fr.afcepf.al29.groupem.dto.ReceiveStatutDto;
 import fr.afcepf.al29.groupem.entities.Address;
 import fr.afcepf.al29.groupem.entities.Order;
 import fr.afcepf.al29.groupem.entities.OrderLine;
@@ -41,6 +56,10 @@ public class EspaceClientController {
 	private String messageAddress;
 	private Address address;
 	private List<Address> listAddress;
+	
+	//variables from WSLogi
+	private Map<Integer, List<String>> statutLines;
+	private List<Integer> keyStatutLines;
 	
 	@Autowired
 	UserBusApi userBusApi;
@@ -85,6 +104,75 @@ public class EspaceClientController {
 		
 		messageAddress="";
 		
+	}
+	
+	public void initStatutViewer(ComponentSystemEvent e){
+		String trackingCode = getParam("trackingCode");
+		String urlWSLogi = "http://localhost:8080/WebServiceLogistique/StatutLivraison/send";
+		JSONObject returnWSLogiJson = null;
+		
+		JSONObject statutRequestJson = new JSONObject();
+		statutRequestJson.put("trackingCode", trackingCode);
+		
+		System.out.println("\n----------\n.: Call: WS Logistique - sendStatut :.");
+		
+		try {
+			URL url = new URL(urlWSLogi);
+			URLConnection connection = url.openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+			System.out.println("Request sent to WS Logistique : " + statutRequestJson.toString());
+			out.write(statutRequestJson.toString());
+			out.close();
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+			String sortie = "";
+			
+			while ((line = in.readLine()) != null) {
+				sortie += line;
+			}
+			
+			System.out.println("Response from WS Logistique : " + sortie );
+			
+			try {//get les élément attendu
+				returnWSLogiJson = new JSONObject(sortie);
+				JSONArray arr = returnWSLogiJson.getJSONArray("statutLines");
+				for (int i = 0; i < arr.length(); i++)
+				{
+				    String dateUpdate = arr.getJSONObject(i).getString("post_id");//A continuer!!!
+				}
+				//ReceiveStatutDto wsLogiResponse = new ObjectMapper().readValue(returnWSLogiJson, ReceiveStatutDto.class);
+				//statutLines = (Map<Integer, List<String>>) returnWSLogiJson.get("statutLines");
+				//statutLines = wsLogiResponse.getStatutLines();
+				keyStatutLines = new ArrayList<>(statutLines.keySet());
+			} catch (JSONException err) {
+				System.out.println("Error with WS Bank :\n\tJSONException on response from WS Logistique");
+				err.printStackTrace();
+			}
+		} catch (MalformedURLException err) {
+			System.out.println("Error with WS Bank :\n\tMalformedURLException");
+			err.printStackTrace();
+		} catch (IOException err) {
+			System.out.println("Error with WS Bank :\n\tIOException");
+			err.printStackTrace();
+		}
+	}
+	
+	public String goToStatutViewer(){
+		String trackingCode = getParam("trackingCode");
+		
+		return "statutViewer?faces-redirect=true&trackingCode=" + trackingCode;
+	}
+	
+	public String getParam(String param){
+		Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String result = params.get(param);
+		
+		return result;
 	}
 	
 	public String userModify(){
@@ -176,5 +264,17 @@ public class EspaceClientController {
 	}
 	public void setMessageOldOrder(String messageOldOrder) {
 		this.messageOldOrder = messageOldOrder;
-	}	
+	}
+	public Map<Integer, List<String>> getStatutLines() {
+		return statutLines;
+	}
+	public void setStatutLines(Map<Integer, List<String>> statutLines) {
+		this.statutLines = statutLines;
+	}
+	public List<Integer> getKeyStatutLines() {
+		return keyStatutLines;
+	}
+	public void setKeyStatutLines(List<Integer> keyStatutLines) {
+		this.keyStatutLines = keyStatutLines;
+	}
 }
